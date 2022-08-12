f1002531c2e79ef05d699d0e8fdc919b58909abd
Damir Akhmadullin <damir@damir.local>
2022-08-12 10:59:20 +0300

    added basic page-not-found

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css'
 import Contacts from 'pages/Contacts'
 import { Routes, Route } from 'react-router-dom'
+import PageNotFound from 'pages/NotFound'
 
 const App: React.FC = () => (
   <Routes>
-    <Route path='/' element={<Contacts />} />
+    <Route path="/" element={<Contacts />} />
+    <Route path="*" element={<PageNotFound />} />
   </Routes>
 )
 export default App

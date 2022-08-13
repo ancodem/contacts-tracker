fca17b460ce4fffbb72b6a07f09917bc8590aadb
Damir Akhmadullin <damir@damir.local>
2022-08-13 13:15:07 +0300

    completed store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import contactsReducer from 'redux/mainReducer'
 
 export const store = configureStore({
   reducer: {
-
+    contacts: contactsReducer
   }
 })
 

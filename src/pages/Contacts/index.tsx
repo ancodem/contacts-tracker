ad49db96330dbedc924b2e17c8bf06ced9e6c6b9
Damir Akhmadullin <damir@damir.local>
2022-08-15 18:58:56 +0300

    Modified contacts/index.tsx
    - Grouped logic
    - Added some comments

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,17 +1,19 @@
-import { Card, Container, CardContent, Typography, Button } from '@mui/material'
-import { useAppSelector } from 'hooks'
-import { Contact } from 'redux/mainReducer'
 import React, { useState } from 'react'
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
+import { Card, Container, CardContent, Typography, Button } from '@mui/material'
+import { Contact } from 'redux/mainReducer'
+import { useAppSelector } from 'hooks'
 import ModalWindow from 'components/ModalWindow'
 
 const Contacts: React.FC = () => {
+
+  // STATE ------------------------------------------------------------------>
   const contacts: Array<Contact> = useAppSelector(
-    state => state.contacts
+    state => state.main.contactsList
   )
 
+  // MODAL WINDOW CONTROLS -------------------------------------------------->
   const [open, setOpen] = useState(false)
-
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 

86bdb2b5aa3df8648fc00d6c359e095f2d741b54
Damir Akhmadullin <damir@damir.local>
2022-08-15 16:31:17 +0300

    modified ModalWindow

--- src/components/ModalWindow.tsx
+++ src/components/ModalWindow.tsx
@@ -1,27 +1,85 @@
 import React from 'react'
-import { Box, Input, Modal } from '@mui/material'
+import { Paper, Button, FormGroup, Input, Modal } from '@mui/material'
+import { addContact, changeNameInput, changePhoneInput } from 'redux/mainReducer'
+import { useAppSelector, useAppDispatch } from 'hooks'
 
 interface ModalProps {
   open: boolean
+  handleClose: () => void
 }
 
-const ModalWindow: React.FC<ModalProps> = ({ open }) => {
+const ModalWindow: React.FC<ModalProps> = ({ open, handleClose }) => {
+  const dispatch = useAppDispatch()
+
+  // STATE ------------------------------------------------------------------>
+  const { name, phoneNumber } = useAppSelector(
+    state => ({
+      name: state.main.contactInput.name,
+      phoneNumber: state.main.contactInput.phoneNumber,
+    })
+  )
+
+  // INPUTS ----------------------------------------------------------------->
+  const onNameChange = (input: string) => {
+    dispatch(changeNameInput(input))
+  }
+
+  const onPhoneNumberChange = (input: string) => {
+    dispatch(changePhoneInput(input))
+  }
+
+  // BUTTON ----------------------------------------------------------------->
+  const handleAddingContact = () => {
+    dispatch(addContact())
+    handleClose()
+  }
+
   return (
-    <>
+    <aside>
       <Modal
         open={open}
+        onClose={() => handleClose()}
+        sx={{
+          display: 'flex',
+          height: '100vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
       >
-        <Box
+        <Paper
+          elevation={10}
           sx={{
+            padding: '2em',
             width: '300px',
-            height: '150px',
+            height: '200px',
           }}
         >
-          <Input placeholder='Name' />
-          <Input placeholder='Phone Number' />
-        </Box>
+          <FormGroup
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-around',
+              height: '10em'
+            }}
+          >
+            <Input
+              onChange={(e) => onNameChange(e.currentTarget.value)}
+              value={name}
+              placeholder='Name' />
+            <Input
+              type='tel'
+              onChange={(e) => onPhoneNumberChange(e.currentTarget.value)}
+              value={phoneNumber}
+              placeholder='Phone Number' />
+            <Button
+              type="submit"
+              onClick={handleAddingContact}
+              variant="outlined">
+              Add
+            </Button>
+          </FormGroup>
+        </Paper>
       </Modal>
-    </>
+    </aside>
   )
 }
 

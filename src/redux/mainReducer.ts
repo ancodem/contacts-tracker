162690f4386eb82889119a9c05e0c2917cd654ec
Damir Akhmadullin <damir@damir.local>
2022-08-15 16:50:21 +0300

    Modified BLL logic for contacts
    - changing name input
    - changing phoneNumber input
    - clear inputs on adding contact

--- src/redux/mainReducer.ts
+++ src/redux/mainReducer.ts
@@ -4,25 +4,52 @@ export interface Contact {
   name: null | string
   phoneNumber: null | string
 }
-type StateType = Array<Contact>
 
-const initialState: StateType = [
-  { name: 'John Doe', phoneNumber: '333-555-321' },
-  { name: 'Tu Pac', phoneNumber: '234-1234-234' },
-  { name: 'Basta Rhymes', phoneNumber: '324-123-567' },
-  { name: 'LeBron James', phoneNumber: '456-345-265' },
-  { name: 'Marshal Matters', phoneNumber: '678-5634-12' },
-]
+interface StateType {
+  contactInput: Contact
+  contactsList: Array<Contact>
+}
+
+const initialState: StateType = {
+  contactInput: {
+    name: '',
+    phoneNumber: '',
+  },
+  contactsList: [
+    { name: 'John Doe', phoneNumber: '333-555-321' },
+    { name: 'Tu Pac', phoneNumber: '234-1234-234' },
+    { name: 'Basta Rhymes', phoneNumber: '324-123-567' },
+    { name: 'LeBron James', phoneNumber: '456-345-265' },
+    { name: 'Marshal Matters', phoneNumber: '678-5634-12' },
+  ]
+}
+
 const contactsSlice = createSlice({
   name: 'contacts-main-slice',
   initialState,
   reducers: {
-    addContact: (state, action: PayloadAction<Contact>) => {
-      state.push(action.payload)
+    addContact: (state) => {
+      state.contactsList.push({ ...state.contactInput })
+      state.contactInput.name = initialState.contactInput.name
+      state.contactInput.phoneNumber = initialState.contactInput.phoneNumber
     },
+    changeNameInput: (
+      state, action: PayloadAction<string>) => {
+      // eslint-disable-next-line
+      state.contactInput.name = action.payload
+    },
+    changePhoneInput: (
+      state, action: PayloadAction<string>) => {
+      // eslint-disable-next-line
+      state.contactInput.phoneNumber = action.payload
+    }
   }
 })
 
-export const { addContact } = contactsSlice.actions
+export const {
+  addContact,
+  changeNameInput,
+  changePhoneInput
+} = contactsSlice.actions
 
 export default contactsSlice.reducer

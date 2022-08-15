98db7495d6772daf6626fa481a23697891998c31
Damir Akhmadullin <damir@damir.local>
2022-08-15 20:02:58 +0300

    styled component file for contacts/index.tsx

---
+++ src/pages/Contacts/styles.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled'
+import { Container } from '@mui/material'
+
+const ContactsContainenr = styled(Container)(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'justify',
+  margin: '30px auto',
+  padding: '40px',
+  flexDirection: 'row',
+  flexWrap: 'wrap',
+  border: `solid 1px blue`,
+})) as typeof Container
+
+export default ContactsContainenr

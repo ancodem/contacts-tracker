77a7469f539b78191f3c6ad5cacb1259ea3a814f
Damir Akhmadullin <damir@damir.local>
2022-08-15 14:47:43 +0300

    Added modal window into cantcts
    - added button to add contacts
    - modal window is displayed upon click
    - you can write down the name and phone number of your new contact

--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -1,13 +1,20 @@
-import { Card, Container, CardContent, Typography } from '@mui/material'
+import { Card, Container, CardContent, Typography, Button } from '@mui/material'
 import { useAppSelector } from 'hooks'
 import { Contact } from 'redux/mainReducer'
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace'
+import ModalWindow from 'components/ModalWindow'
 
 const Contacts: React.FC = () => {
   const contacts: Array<Contact> = useAppSelector(
     state => state.contacts
   )
+
+  const [open, setOpen] = useState(false)
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   return (
     <Container sx={{
       display: 'flex',
@@ -16,6 +23,12 @@ const Contacts: React.FC = () => {
       flexDirection: 'column',
       alignItems: 'center'
     }}>
+      <Button
+        onClick={handleOpen}
+        variant="outlined">
+        Add
+      </Button>
+      <ModalWindow open={open} handleClose={handleClose} />
       {contacts.map((contact: Contact): ReactJSXElement =>
         <Card key={contact.name}
           sx={{

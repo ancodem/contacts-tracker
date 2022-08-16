5df4f4bfaf5ed96dc3474dde7c0927c297f5473b
Damir Akhmadullin <damir@damir.local>
2022-08-16 20:13:51 +0300

    Added ContactCard icons
    - edit icon
    - delete icon

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -1,10 +1,40 @@
 import React from 'react'
 import { CardContent, Typography } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import { Contact } from 'redux/mainReducer'
 import CardContainer from './styles'
 
 const ContactCard: React.FC<Contact> = ({ name, phoneNumber }) => (
   <CardContainer>
+    <EditIcon
+      sx={{
+        position: 'absolute',
+        right: '1px',
+        bottom: '1px',
+        alignSelf: 'flex-end',
+        borderRadius: '20px',
+        padding: '3px',
+        color: 'gray',
+        '&:hover': {
+          color: 'blue'
+        }
+      }}
+    />
+    <HighlightOffIcon
+      sx={{
+        position: 'absolute',
+        right: '1px',
+        top: '-1px',
+        alignSelf: 'flex-end',
+        borderRadius: '20px',
+        padding: '3px',
+        color: 'gray',
+        '&:hover': {
+          color: 'blue'
+        }
+      }}
+    />
     <CardContent>
       <Typography sx={{ fontSize: '10pt', color: 'gray' }}>
         {name}

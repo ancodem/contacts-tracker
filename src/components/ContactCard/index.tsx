50541c412ec0032b4260a47f8486bca39c960b1e
Damir Akhmadullin <damir@damir.local>
2022-08-16 20:47:17 +0300

    styled icons

--- src/components/ContactCard/index.tsx
+++ src/components/ContactCard/index.tsx
@@ -1,40 +1,12 @@
 import React from 'react'
 import { CardContent, Typography } from '@mui/material'
-import EditIcon from '@mui/icons-material/Edit'
-import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import { Contact } from 'redux/mainReducer'
-import CardContainer from './styles'
+import { CardContainer, EditContactIcon, DeleteContactIcon } from './styles'
 
 const ContactCard: React.FC<Contact> = ({ name, phoneNumber }) => (
   <CardContainer>
-    <EditIcon
-      sx={{
-        position: 'absolute',
-        right: '1px',
-        bottom: '1px',
-        alignSelf: 'flex-end',
-        borderRadius: '20px',
-        padding: '3px',
-        color: 'gray',
-        '&:hover': {
-          color: 'blue'
-        }
-      }}
-    />
-    <HighlightOffIcon
-      sx={{
-        position: 'absolute',
-        right: '1px',
-        top: '-1px',
-        alignSelf: 'flex-end',
-        borderRadius: '20px',
-        padding: '3px',
-        color: 'gray',
-        '&:hover': {
-          color: 'blue'
-        }
-      }}
-    />
+    <EditContactIcon />
+    <DeleteContactIcon />
     <CardContent>
       <Typography sx={{ fontSize: '10pt', color: 'gray' }}>
         {name}
--- src/components/ContactCard/styles.tsx
+++ src/components/ContactCard/styles.tsx
@@ -1,8 +1,11 @@
 import styled from '@emotion/styled'
 import { Card } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 
-const CardContainer = styled(Card)(({ theme }) => ({
+export const CardContainer = styled(Card)(({ theme }) => ({
   width: '10em',
+  position: 'relative',
   margin: '10px 20px',
   border: 'solid 1px white',
   '&:hover': {
@@ -12,4 +15,28 @@ const CardContainer = styled(Card)(({ theme }) => ({
   }
 }))
 
-export default CardContainer
+export const EditContactIcon = styled(EditIcon)(({ theme }) => ({
+  position: 'absolute',
+  right: '1px',
+  bottom: '1px',
+  alignSelf: 'flex-end',
+  borderRadius: '20px',
+  padding: '3px',
+  color: '#efebe9',
+  '&:hover': {
+    color: '#bcaaa4'
+  }
+}))
+
+export const DeleteContactIcon = styled(HighlightOffIcon)(({ theme }) => ({
+  position: 'absolute',
+  right: '1px',
+  top: '1px',
+  alignSelf: 'flex-end',
+  borderRadius: '20px',
+  padding: '3px',
+  color: '#efebe9',
+  '&:hover': {
+    color: '#ff1744'
+  }
+}))
